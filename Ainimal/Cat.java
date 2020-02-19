683cc13b6d6e808b3b4b13d43d73fcee47fa3492
fengzhizi <2270863513@qq.com>
2020-02-19 11:47:00 +0800

    第一次提交

---
+++ Ainimal/Cat.java
@@ -0,0 +1,16 @@
+package Ainimal;
+
+public class Cat extends Ainimal {
+
+    public Cat() {
+    }
+
+    public Cat(int age, String name) {
+        super(age, name);
+    }
+    public void catchmouse()
+    {
+        System.out.println("猫爪老鼠");
+    }
+}
+
---
+++ Ainimal/Dog.java
@@ -0,0 +1,15 @@
+package Ainimal;
+
+public class Dog  extends Ainimal{
+    public Dog() {
+    }
+
+    public Dog(int age, String name) {
+        super(age, name);
+
+    }
+    public void lookdoor()
+    {
+        System.out.println("狗看门");
+    }
+}
